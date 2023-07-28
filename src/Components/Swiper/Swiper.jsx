6c02f7351f2cc808d2cf095deaa9d7a9298392f5
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-07-28 22:02:44 +0000

    add to swiper

---
+++ src/Components/Swiper/Swiper.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+
+import "swiper/css";
+import img1 from '../../img/swiper1.png';
+import img2 from '../../img/swiper2.png';
+
+export default function Hero() {
+
+    return (
+
+        <section data-aos="fade-up" className="mb-10">
+            <Swiper className="w-full relative cursor-pointer"
+                slidesPerView={2}
+                loop
+                // onInit={(ev) => {
+                //     set_swiper(ev);
+                // }}
+                breakpoints={{
+                    // when window width is >= 640px
+                    640: {
+                        slidesPerView: 2,
+                    },
+                    // when window width is >= 768px
+                    868: {
+                        slidesPerView: 2,
+                    },
+                }}
+            >
+                <SwiperSlide >
+                    <img style={{width: '100%', height:'autu', padding: '0', margin: '0' }} src={img1} alt="slide" />
+                </SwiperSlide>
+
+                <SwiperSlide >
+                    <img style={{width: '100%', height: 'auto'}} src={img2} alt="slide" />
+                </SwiperSlide>
+
+                <SwiperSlide >
+                    <img style={{width: '100%', height: 'auto'}} src={img1} alt="slide" />
+                </SwiperSlide>
+
+                <SwiperSlide >
+                    <img style={{width: '100%', height: 'auto'}} src={img2} alt="slide" />
+                </SwiperSlide>
+
+            </Swiper>
+
+            {/* <News /> */}
+
+        </section>
+    )
+}
