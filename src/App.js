7eedbda5ab832011235dfb6dc290f6f189d8c1e2
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-07-28 20:58:59 +0000

    navbar change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,159 @@
 
+import React from 'react';
+import Navbar from './Components/Navbar';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './style/style.css'
+import { useState } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom';
+
+
+// import Zavtrk from './pages/zavtrk/Zavtrk';
+
+
+// import ZavrtrkDetail from './pages/zavtrk/ZavrtrkDetail';
+// import Footer from './pages/Footer';
+// import Novinki from './Components/Novinki';
+// import OpenNovinki from './Components/OpenNovinki';
+
+
+
+// import Pitsa from './pages/pitsa/Pitsa';
+
+
+// import Pasta from './pages/pasta/Pasta';
+
+// import Ravioli from './pages/ravioli/Ravioli';
+
+
+// import Supi from './pages/supi/Supi';
+
+
+// import Zakiska from './pages/Горячие закуски/Zakiska';
+
+
+// import Salat from './pages/salat/Salat';
+
+
+
+
 
 function App() {
+
+  
+  const [detail, setDetail] = useState(null);
+
+
+  
+  // const [detailed, setDetailed] = useState(null);
+
+ 
+
+  const [pissa, setPissa] = useState(null);
+
+ 
+
+  const [past, setPast] = useState(null);
+
+
+  
+  const [ravi, setRavi] = useState(null);
+
+
+  
+  const [sup, setSup] = useState(null);
+
+  
+  const [zaks, setZaks] = useState(null);
+
+
+  
+
+  const [alivya, setAlivya] = useState(null);
+
+  
+
+  const [napitki, setNapitki] = useState(null);
+
+  const [orders, setOrders] = useState([])
+  const [count, setCount] = useState(0)
+
+  const addToCart = (obj) => {
+    let isDefined = false;
+    for (let o of orders) {
+      if (o.id === obj.id) {
+        isDefined = true;
+      }
+    }
+    if (!isDefined) {
+      setCount(count + obj.price)
+      setOrders([...orders, obj])
+    }
+  }
+
+  const deleteOrderItem = (order) => {
+    setOrders(orders.filter(o => o.id !== order.id));
+    setCount(count - order.price)
+  }
+
+
+ 
+
+
   return (
-    <div className="App">
+
+    <BrowserRouter>
       
-    </div>
-  );
+        <div className='stikky'>
+          <Navbar orders={orders} setCount={setCount} count={count} deleteOrderItem={deleteOrderItem} />
+        </div>
+
+          <Routes>
+            {/* <Route path='/' element={<Novinki novinki={novinki} addToCart={addToCart} setDetail={setDetail} pitsa={pitsa} setPissa={setPissa} />} />
+            <Route path='/novinki/:id' element={<OpenNovinki novinki={novinki} addToCart={addToCart} detail={detail} />} />
+            <Route path='/pitsa/:id' element={<OpenPitsa addToCart={addToCart} pissa={pissa} pitsa={pitsa} />} />
+
+
+            <Route path='/zavtrks' element={<Zavtrk addToCart={addToCart} zavtrks={zavtrks} setDetailed={setDetailed} />} />
+            <Route path='/zavtrks/:id' element={<ZavrtrkDetail addToCart={addToCart} zavtrks={zavtrks} detailed={detailed} />} />
+
+            <Route path='/pitsa' element={<Pitsa addToCart={addToCart} pitsa={pitsa} setPissa={setPissa} />} />
+
+            <Route path='/pitsa/:id' element={<PitsaDetail addToCart={addToCart} pissa={pissa} pitsa={pitsa} />} />
+
+            <Route path='/pasta' element={<Pasta addToCart={addToCart} pasta={pasta} setPast={setPast} />} />
+            <Route path='/pasta/:id' element={<PastaDetailed addToCart={addToCart} pasta={pasta} past={past} />} />
+
+            <Route path='/ravioli' element={<Ravioli addToCart={addToCart} setRavi={setRavi} ravioli={ravioli} />} />
+            <Route path='/ravioli/:id' element={<Raviolidetail addToCart={addToCart} ravi={ravi} ravioli={ravioli} />} />
+
+            <Route path='/supi' element={<Supi addToCart={addToCart} setSup={setSup} supi={supi} />} />
+            <Route path='/supi/:id' element={<SupiDetail addToCart={addToCart} supi={supi} sup={sup} />} />
+
+            <Route path='/zakiska' element={<Zakiska addToCart={addToCart} setZaks={setZaks} zakus={zakus} />} />
+            <Route path='/zakiska/:id' element={<ZakiskaDetail addToCart={addToCart} zaks={zaks} zakus={zakus} />} />
+
+            <Route path='/salat' element={<Salat addToCart={addToCart} setAlivya={setAlivya} salat={salat} />} />
+            <Route path='/salat/:id' element={<SalatDetail alivya={alivya} addToCart={addToCart} salat={salat} />} />
+
+            <Route path='/suv' element={<Napitki addToCart={addToCart} suv={suv} setNapitki={setNapitki} />} />
+            <Route path='/suv/:id' element={<NapitkiDetail addToCart={addToCart} suv={suv} napitki={napitki} />} />
+
+            <Route path='/kontact' element={<Kontact />} />
+            <Route path='/card' element={<Card orders={orders} deleteOrderItem={deleteOrderItem} count={count} />} />
+            <Route path='/dastavka' element={<Daastavka />} />
+            <Route path='/po_card' element={<Po_card orders={orders} />} /> */}
+
+          </Routes>
+
+        {/* <Footer /> */}
+
+    </BrowserRouter >
+
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,250 @@
+import React, { useEffect } from 'react'
+import logoo from '../img/logo2.png';
+import img from '../img/img.png';
+import { Link, NavLink } from 'react-router-dom';
+import { useRef } from 'react';
+import { useState } from 'react';
+
+
+function Navbar(props) {
+
+    const open = useRef();
+    const openRegistr = useRef();
+    const [count, setCount] = useState(1);
+    const closeKorzinka = (order) => {
+        props.deleteOrderItem(order)
+        props.setCount(props.count - order.price)
+    }
+    const rolls = useRef()
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const closcard = () => {
+        rolls.current.style.display = 'none'
+    }
+    useEffect(() => {
+        return;
+    }, [count])
+
+    useEffect(() => {
+        if (props.orders.length === 0) {
+            localStorage.setItem('total', 0)
+        }
+    }, [])
+
+    const [oren, setOren] = useState(false);
+
+
+    const openOrder = () => {
+        setOren(prevState => !prevState)
+    }
+
+    const vhod = () => {
+        open.current.style.display = 'block'
+    }
+
+    const close = () => {
+        open.current.style.display = 'none'
+    }
+
+    const closeRegistr = () => {
+        openRegistr.current.style.display = 'none'
+    }
+
+    const registr = () => {
+        openRegistr.current.style.display = 'block'
+    }
+
+    let getTotalPrice = [];
+
+
+    let total = 0;
+
+    props.orders.forEach(i => {
+        if (i.stock > 1) {
+            total += i.price * i.stock
+
+        } else {
+            total += i.price
+        }
+    });
+
+
+    return (
+        <div className='big'>
+            <div className='container'>
+                <div className='nav'>
+                    <div className='c-nav'>
+                        <div><img src={logoo} alt="img" /></div>
+                        <div className='logo'>
+                            <div className='dostavki'>
+                                <p className='pop'>
+                                    Доставка пасты <span style={{ color: '#F7D22D' }}>Москва</span>
+                                </p>
+                                <div>
+                                    <img src={img} alt="img" />
+                                    <p>Яндекс еда-4.8</p>
+                                    <i style={{ padding: '0 15px 0 6px' }} className="fa-solid fa-star"></i>
+                                    <p>Время доставки-от 31 мин</p>
+                                </div>
+                            </div>
+                            <div className='zakaz'>
+                                <button className='bnt' onClick={registr}>Заказать звонок</button>
+                                <p>8 916 234 06 58</p>
+                            </div>
+                        </div>
+
+                        <i onClick={() => setIsOpen(!isOpen)} className={`bi ${!isOpen ? " bi-list" : "bi-x"}`}></i>
+
+                    </div>
+                    <div className={`nav-footer ${isOpen ? "nav-open" : "nav-close"} `}>
+                        <ul onClick={() => setIsOpen(false)}>
+                            <li >
+                                <Link to='/zavtrks'>Завтраки</Link>
+                            </li>
+                            <li >
+                                <Link to='/pitsa'>Пицца</Link>
+                            </li>
+                            <li >
+                                <Link to={'/pasta'}>Паста</Link>
+                            </li>
+                            <li >
+                                <Link to={'/ravioli'}>Равиоли</Link>
+                            </li>
+                            <li >
+                                <Link to={'/supi'}>Супы</Link>
+                            </li>
+                            <li >
+                                <Link to={'/zakiska'}>Горячие закуски</Link>
+                            </li>
+                            <li >
+                                <Link to={'/salat'}>Салаты</Link>
+                            </li>
+                            <li >
+                                <Link to={'/suv'}>Напитки</Link>
+                            </li>
+                            <li >
+                                <Link to='/kontact'>Контакты</Link>
+                            </li>
+                            <li >
+                                <a href="#" onClick={vhod}>Войти</a>
+                            </li>
+                        </ul>
+
+
+                        <div className='pols'>
+                            <button className='pols_btn' onClick={openOrder}>Корзина | {props.orders.length}</button>
+                            {oren
+                                ? <div ref={rolls} className='rols' >
+                                    {props.orders.length !== 0
+                                        ? props.orders.map((order, index) => {
+                                            return (
+                                                <div key={order.id}>
+                                                    <hr />
+                                                    <div className="korzina" >
+                                                        {getTotalPrice.push(props.orders[index])}
+                                                        <img src={order.img} alt="Error" />
+                                                        <div className="tittle">
+                                                            <div className='title_body'>
+                                                                <p className='krem'>{order.name}</p>
+                                                                <div className='circle' onClick={() => closeKorzinka(order)}>
+                                                                    <p>&times;</p>
+                                                                </div>
+                                                            </div>
+                                                            <div className="k-footer">
+                                                                <div className="four">
+                                                                    <div className='minus' onClick={() => {
+                                                                        order.stock >= 1 && setCount(order.stock--)
+                                                                        localStorage.setItem('total', getTotalPrice.reduce((acc, item) => acc + (item.price * item.stock), 0))
+                                                                    }} >-</div>
+                                                                    <p className='count'>{order.stock >= 1 ? order.stock : 1}</p>
+                                                                    <div className='plus' onClick={() => {
+                                                                        setCount(order.stock++)
+                                                                        localStorage.setItem('total', getTotalPrice.reduce((acc, item) => acc + (item.price * item.stock), 0))
+                                                                    }}>+</div>
+                                                                </div>
+                                                                <p className='price'>{order.stock > 1 ? order.price * order.stock : order.price} ₽</p>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            )
+                                        })
+
+                                        : <div className='no_date'>
+                                            <p>No results</p>
+                                        </div>
+                                    }
+
+                                    {props.orders.length !== 0
+                                        ? <div className="kor-foter" >
+                                            <div className="foter-header">
+                                                <p>Сумма заказа</p>
+                                                <p>{total} ₽</p>
+                                            </div>
+                                            <div className="kor_foter">
+                                                <NavLink to={'/card'}>
+                                                    <button onClick={closcard}>В корзину</button>
+                                                </NavLink>
+                                                <NavLink to={'/dastavka'}>
+                                                    <button onClick={closcard}>Оформить</button>
+                                                </NavLink>
+                                            </div>
+                                        </div>
+                                        : null}
+                                </div>
+                                : null}
+
+                        </div>
+
+                        <div className="clow" ref={open}>
+                            <div id='div'>
+                                <div className="srov">
+                                    <div className="wrapper">
+                                        <p>Вход на сайт</p>
+                                        <p onClick={close}>&times;</p>
+                                    </div>
+                                    <div className="inp">
+                                        <p>Номер телефона</p>
+                                        <input type="number" />
+                                    </div>
+                                    <div className="clow-footer">
+                                        <button>Выслать код</button>
+                                        <p>
+                                            Продолжая, вы соглашаетесь со сбором и обработкой
+                                            персональных данных и пользовательским соглашением
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div ref={openRegistr} className="clow">
+                            <div id='div'>
+                                <div className="srov">
+                                    <div className="wrapper">
+                                        <p>Заказать звонок</p>
+                                        <p onClick={closeRegistr}>&times;</p>
+                                    </div>
+                                    <div className="inp">
+                                        <p>Номер телефона</p>
+                                        <input type="number" />
+                                    </div>
+                                    <div className="clow-footer">
+                                        <button>Заказать</button>
+                                        <p>
+                                            Продолжая, вы соглашаетесь со сбором и обработкой
+                                            персональных данных и пользовательским соглашением
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
+    )
+}
+
+export default Navbar
