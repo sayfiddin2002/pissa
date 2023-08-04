2694608b6fa1b18574c571dbdccdbd47b3ea1903
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-08-04 14:11:09 +0000

    add ravioli page

---
+++ src/pages/ravioli/Ravioli.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { ravioli } from '../../data'
+
+function Ravioli(props) {
+  return (
+    <div className='pas-foot'>
+      <h1>Равиоли</h1>
+      <div className="container">
+        {ravioli.map(ravi => {
+          return (
+            <div data-aos="fade-up" className="card" key={ravi.id}>
+              <NavLink to={`/ravioli/${ravi.id}`}>
+                <div onClick={()=> props.setRavi(ravi)} className="image">
+                  <img src={ravi.img} alt="Error" />
+                </div>
+              </NavLink>
+              <div className="card-foter">
+                <h3>{ravi.name.slice(0, 20)}...</h3>
+                <p>{ravi.description.slice(0, 60)}...</p>
+                <div className="pas-footer">
+                  <p>{ravi.price} ₽</p>
+                  <button onClick={() => props.addToCart(ravi)}>Купить</button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Ravioli
