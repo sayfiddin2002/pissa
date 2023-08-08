f25957b343b3337043fd22b1c80371c5fc2e75f3
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-08-08 10:32:51 +0000

    add suv page

---
+++ src/pages/suv/Suv.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { suv } from '../../data'
+
+function Suv(props) {
+    return (
+        <div  className='pas-foot'>
+            <h1>Напитки</h1>
+            <div className="container">
+                {suv.map(s => {
+                    return (
+                        <div data-aos="fade-up" className="card" key={s.id}>
+                            <NavLink to={`/suv/${s.id}`}>
+                                <div onClick={()=> props.setNapitki(s)} className="image">
+                                    <img src={s.img} alt="Error" />
+                                </div>
+                            </NavLink>
+                            <div className='card-foter'>
+                                <h3>{s.name.slice(0, 20)}...</h3>
+                                <p>{s.description.slice(0, 60)}...</p>
+                                <div className="pas-footer">
+                                    <p>{s.price} ₽</p>
+                                    <button onClick={() => props.addToCart(s)}>Купить</button>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Suv
