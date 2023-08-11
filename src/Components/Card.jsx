e3058a68af2e9b8fe99446ec5e994b98aa6bce97
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-08-11 14:43:35 +0000

    add card page

---
+++ src/Components/Card.jsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Card(props) {
+  const [count, setCount] = useState(1)
+  let getTotalPrice = [];
+
+  let total = 0;
+
+  props.orders.forEach(i => {
+    if (i.stock > 1) {
+      total += i.price * i.stock
+
+    } else {
+      total += i.price
+    }
+  });
+
+  return (
+    <div className='v_korzinu'>
+      <div className="corzina_header">
+        <div className="item_one">
+          <div className="one">
+            <p>1</p>
+          </div>
+          <p className="text">Корзина</p>
+        </div>
+        <div className="item_two">
+          <div className="two">
+            <p>2</p>
+          </div>
+          <p className="text">Оформление заказа</p>
+        </div>
+        <div className="item_three">
+          <div className="three">
+            <p>3</p>
+          </div>
+          <p className="text">Заказ принят</p>
+        </div>
+      </div>
+
+      <div className='cozinu'>
+        <h1 className='text_corzina'>Корзины</h1>
+        <div className='wrapper'>
+          {props.orders.map((order, index) => {
+            return (
+              <div key={order.index}>
+                <hr />
+                <div className="card">
+                  {getTotalPrice.push(props.orders[index])}
+                  <div className="card_header">
+                    <img src={order.img} alt="Error" />
+                    <div className="text">
+                      <p>{order.name}</p>
+                      <p>{order.description.slice(1, 100)}</p>
+                    </div>
+                  </div>
+                  <div className="card-body">
+                    {/* <p className='price'>{order.price}₽</p> */}
+                    <p className='price'>{order.stock > 1 ? order.price * order.stock : order.price} ₽</p>
+                    <div className='four'>
+                      <div className='minus' onClick={() => order.stock >= 1 && setCount(order.stock--)}>-</div>
+                      <p className='count'>{order.stock >= 1 ? order.stock : 1}</p>
+                      <div className='plus' onClick={() => setCount(order.stock++)}>+</div>
+                    </div>
+                    <div className='circle' onClick={() => props.deleteOrderItem(order)}>
+                      <p >&times;</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="card-footer">
+          <div className="footer_wrapper">
+            <div className="footer-header">
+              <div className="summa">
+                <p>Сумма заказа:</p>
+                <p>{total} ₽</p>
+              </div>
+            </div>
+          </div>
+          <div className="magazin">
+            <div className="vernutsya">
+              <i class="bi bi-arrow-left"></i>
+              <NavLink to={'/'}>
+                <p> Вернуться в магазин</p>
+              </NavLink>
+            </div>
+            <div className="ofor">
+              <NavLink to={'/dastavka'}>
+                <button>Оформление заказа <i className="fa-solid fa-chevron-right"></i></button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Card
