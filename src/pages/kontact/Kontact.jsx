6825621ec8128096a4aafacbb4a8ac82c32d8544
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-08-12 16:33:16 +0000

    changed the font-size of a in the footer

--- src/pages/kontact/Kontact.jsx
+++ src/pages/kontact/Kontact.jsx
@@ -9,7 +9,7 @@ function Kontact() {
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2250.588803794508!2d37.477642515926995!3d55.661360580528985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54da002656bc7%3A0xa799933657c1d4f!2z0L_RgC4g0JLQtdGA0L3QsNC00YHQutC-0LPQviwgODbQsiwg0JzQvtGB0LrQstCwLCAxMTk0MTU!5e0!3m2!1sru!2sru!4v1645970050041!5m2!1sru!2sru">
                     </iframe>
                     <div data-aos="fade-up" className="contakt-footer">
-                        <h1>8 916 234 06 58</h1>
+                        <h1>+998 94 105 57 99</h1>
                         <h2>Москва ул. Проспект Вернадского 86В ( Парк Фудхол)</h2>
                         <div>
                             <p>Доставка и самовывоз:</p>
