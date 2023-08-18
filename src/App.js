1336b792d25772b07bde7d5cc047cf774b10fcf7
Sayfiddin <122016496+sayfiddin2002@users.noreply.github.com>
2023-08-18 14:48:26 +0000

    toast change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import {
   Route
 } from 'react-router-dom';
 
+import { ToastContainer, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+
 
 import ZavtrkDetail from './pages/zavtrk/ZavtrkDetail';
 import Novinki from './Components/Novinki';
@@ -36,42 +40,15 @@ import Daastavka from './pages/dastavka/Dastafka';
 import Po_card from './pages/payment/Payment';
 
 function App() {
-
-  
   const [detail, setDetail] = useState(null);
-
-
-  
   const [detailed, setDetailed] = useState(null);
-
- 
-
   const [pissa, setPissa] = useState(null);
-
- 
-
   const [past, setPast] = useState(null);
-
-
-  
   const [ravi, setRavi] = useState(null);
-
-
-  
   const [sup, setSup] = useState(null);
-
-  
   const [zaks, setZaks] = useState(null);
-
-
-  
-
   const [alivya, setAlivya] = useState(null);
-
-  
-
   const [napitki, setNapitki] = useState(null);
-
   const [orders, setOrders] = useState([])
   const [count, setCount] = useState(0)
 
@@ -80,6 +57,16 @@ function App() {
     for (let o of orders) {
       if (o.id === obj.id) {
         isDefined = true;
+        toast.error('🦄 Wow so easy!', {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
       }
     }
     if (!isDefined) {
@@ -93,53 +80,56 @@ function App() {
     setCount(count - order.price)
   }
 
+
   return (
 
     <BrowserRouter>
-      
-        <div className='stikky'>
-          <Navbar orders={orders} setCount={setCount} count={count} deleteOrderItem={deleteOrderItem} />
-        </div>
 
-          <Routes>
-            <Route path='/' element={<Novinki  addToCart={addToCart} setDetail={setDetail}  setPissa={setPissa} />} />
-            <Route path='/novinki/:id' element={<OpenNovinki  addToCart={addToCart} detail={detail} />} />
-            {/* <Route path='/pitsa/:id' element={<OpenPitsa addToCart={addToCart} pissa={pissa} pitsa={pitsa} />} /> */}
+      <div className='stikky'>
+        <Navbar orders={orders} setCount={setCount} count={count} deleteOrderItem={deleteOrderItem} />
+      </div>
+
+      <Routes>
+        <Route path='/' element={<Novinki addToCart={addToCart} setDetail={setDetail} setPissa={setPissa} />} />
+        <Route path='/novinki/:id' element={<OpenNovinki addToCart={addToCart} detail={detail} />} />
+        {/* <Route path='/pitsa/:id' element={<OpenPitsa addToCart={addToCart} pissa={pissa} pitsa={pitsa} />} /> */}
 
 
-            <Route path='/zavtrks' element={<Zavtrk addToCart={addToCart}  setDetailed={setDetailed} />} />
-            <Route path='/zavtrks/:id' element={<ZavtrkDetail addToCart={addToCart}  detailed={detailed} />} />
+        <Route path='/zavtrks' element={<Zavtrk addToCart={addToCart} setDetailed={setDetailed} />} />
+        <Route path='/zavtrks/:id' element={<ZavtrkDetail addToCart={addToCart} detailed={detailed} />} />
 
-            <Route path='/pitsa' element={<Pitsa addToCart={addToCart}  setPissa={setPissa} />} />
+        <Route path='/pitsa' element={<Pitsa addToCart={addToCart} setPissa={setPissa} />} />
 
-            <Route path='/pitsa/:id' element={<PitsaDetail addToCart={addToCart} pissa={pissa}  />} />
+        <Route path='/pitsa/:id' element={<PitsaDetail addToCart={addToCart} pissa={pissa} />} />
 
-            <Route path='/pasta' element={<Pasta addToCart={addToCart}  setPast={setPast} />} />
-            <Route path='/pasta/:id' element={<PastaDetailed addToCart={addToCart}  past={past} />} />
+        <Route path='/pasta' element={<Pasta addToCart={addToCart} setPast={setPast} />} />
+        <Route path='/pasta/:id' element={<PastaDetailed addToCart={addToCart} past={past} />} />
 
-            <Route path='/ravioli' element={<Ravioli addToCart={addToCart} setRavi={setRavi} />} />
-            <Route path='/ravioli/:id' element={<Raviolidetail addToCart={addToCart} ravi={ravi}  />} />
+        <Route path='/ravioli' element={<Ravioli addToCart={addToCart} setRavi={setRavi} />} />
+        <Route path='/ravioli/:id' element={<Raviolidetail addToCart={addToCart} ravi={ravi} />} />
 
-            <Route path='/supi' element={<Supi addToCart={addToCart} setSup={setSup} />} />
-            <Route path='/supi/:id' element={<SupiDetail addToCart={addToCart} sup={sup} />} />
+        <Route path='/supi' element={<Supi addToCart={addToCart} setSup={setSup} />} />
+        <Route path='/supi/:id' element={<SupiDetail addToCart={addToCart} sup={sup} />} />
 
-            <Route path='/zakiska' element={<Zakiska addToCart={addToCart} setZaks={setZaks} />} />
-            <Route path='/zakiska/:id' element={<ZakiskaDetail addToCart={addToCart} zaks={zaks} />} />
+        <Route path='/zakiska' element={<Zakiska addToCart={addToCart} setZaks={setZaks} />} />
+        <Route path='/zakiska/:id' element={<ZakiskaDetail addToCart={addToCart} zaks={zaks} />} />
 
-            <Route path='/salat' element={<Salat addToCart={addToCart} setAlivya={setAlivya}  />} />
-            <Route path='/salat/:id' element={<SalatDetail alivya={alivya} addToCart={addToCart} />} />
+        <Route path='/salat' element={<Salat addToCart={addToCart} setAlivya={setAlivya} />} />
+        <Route path='/salat/:id' element={<SalatDetail alivya={alivya} addToCart={addToCart} />} />
 
-            <Route path='/suv' element={<Suv addToCart={addToCart} setNapitki={setNapitki} />} />
-            <Route path='/suv/:id' element={<SuvDetail addToCart={addToCart} napitki={napitki} />} />
+        <Route path='/suv' element={<Suv addToCart={addToCart} setNapitki={setNapitki} />} />
+        <Route path='/suv/:id' element={<SuvDetail addToCart={addToCart} napitki={napitki} />} />
 
-            <Route path='/kontact' element={<Kontact />} />
-            <Route path='/card' element={<Card orders={orders} deleteOrderItem={deleteOrderItem} count={count} />} />
-            <Route path='/dastavka' element={<Daastavka />} />
-            <Route path='/po_card' element={<Po_card orders={orders} />} />
+        <Route path='/kontact' element={<Kontact />} />
+        <Route path='/card' element={<Card orders={orders} deleteOrderItem={deleteOrderItem} count={count} />} />
+        <Route path='/dastavka' element={<Daastavka />} />
+        <Route path='/po_card' element={<Po_card orders={orders} />} />
 
-          </Routes>
+      </Routes>
 
-        <Footer />
+      <Footer />
+
+      <ToastContainer />
 
     </BrowserRouter >
 
